0fdb0e972d8225771b4c29c0754876a2f7d8e339
DELL <21AK22@Odif.DELL>
2021-04-19 12:40:12 +0700

    all

---
+++ build/precache-manifest.774591ee26c8bd5830da80ab9b609ed0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b99fd6abf9a9eda44ddffb6842b4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed050b1266a83994a61",
+    "url": "/static/css/2.d4dfd184.chunk.css"
+  },
+  {
+    "revision": "151a18597fcb1055ee92",
+    "url": "/static/css/main.fa87190e.chunk.css"
+  },
+  {
+    "revision": "1ed050b1266a83994a61",
+    "url": "/static/js/2.b0f9acc5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.b0f9acc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151a18597fcb1055ee92",
+    "url": "/static/js/main.9cd0eef3.chunk.js"
+  },
+  {
+    "revision": "dd331eaf5ff50a41d49f",
+    "url": "/static/js/runtime-main.2410f066.js"
+  },
+  {
+    "revision": "81f09f2665830b4cc603adcdf888a531",
+    "url": "/static/media/mobile.81f09f26.svg"
+  }
+]);
